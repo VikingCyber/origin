d922ff33d0dd56b6abdac634e3aa65bdc7902c24
Viking <viking602ki557@mail.ru>
2023-01-22 20:04:34 +0700

    docs_added

--- Pr4.py
+++ Pr4.py
@@ -8,6 +8,31 @@ students = {'ivanov': datetime.datetime(2022, 12, 1),
 
 
 def deadline_score(pass_date, deadline_date):
+    """"
+    Функция для оценивания практической работы
+
+    Функция определяет исходя из даты дедлайна и фактической сдачи,
+    сколько баллов получит студент за сданную работу.
+    Функция принимает на вход два параметра в формате (год, месяц, день)
+    в формате модуля datetime.
+    1)Дату сдачи работы
+    2)Дату дэдлайна работы
+
+    Args:
+        pass_date(class 'datetime.datetime')
+        deadline_date(class 'datetime.datetime')
+    Returns:
+        int: оценка за практическую работу
+    Examples:
+        input valid command:deadline_score
+        pass_year:22
+        pass_month:12
+        pass_day:12
+        year_deadline:22
+        month_deadline:11
+        day_deadline29
+        3
+    """
     data = str(pass_date - deadline_date)
     try:
         data_int = int(data[0] + data[1])
@@ -19,13 +44,29 @@ def deadline_score(pass_date, deadline_date):
         return 4
     elif 8 <= data_int <= 14:
         return 3
-    elif 14 < data_int < 21:
+    elif 14 < data_int <= 21:
         return 2
     elif data_int > 21:
         return 0
 
 
 def late_list(dict_students, deadline_date):
+    """"
+    Студенты просрочевшие дедлайн
+
+    Функия определяет сколько студентов из определённого списка
+    сдали работу позже намеченного срока
+
+    Args:
+        dict_students(dict)
+        deadline_date(class 'datetime.datetime')
+    Returns:
+        list: список с фамилиями студентов, просрочевших дедлайн
+    Example:
+        input valid command:late_list
+        ['bonchbruevich', 'ivanov', 'petrov', 'sidorov']
+
+    """
     result_list = []
     for k, v in dict_students.items():
         if v > deadline_date:
@@ -40,12 +81,12 @@ def main():
             while True:
                 try:
                     pass_date = datetime.datetime(int(input('pass_year:')),
-                                                  int(input('pass_month:')),int(input('pass_day:')))
+                                                  int(input('pass_month:')), int(input('pass_day:')))
                     deadline_date = datetime.datetime((int(input('year_deadline:'))),
                                                       int(input('month_deadline:')), int(input('day_deadline')))
                     break
                 except ValueError:
-                    print('digits is required')
+                    print('digits is required or input format is wrong')
                     continue
             print(deadline_score(pass_date, deadline_date))
         elif input_string == 'late_list':
@@ -59,4 +100,4 @@ if __name__ == '__main__':
     main()
 
 
-# изменения для защиты 
+# изменения для защиты
